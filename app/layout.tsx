626c110feaadafaf256860263d103335df1702f0
Jm <jmdelasalas.dev@gmail.com>
2025-09-17 21:37:45 +0800

    Updated configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import Providers from './providers';
+import { ColorSchemeScript } from '@mantine/core';
 import { Poppins } from 'next/font/google';
 import '@mantine/core/styles.css';
 import 'mantine-datatable/styles.layer.css';
@@ -24,6 +25,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			lang='en'
 			className={poppins.className}
 		>
+			<head>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no'
+				/>
+				<ColorSchemeScript />
+			</head>
 			<body>
 				<Providers>
 					<AppWrapper>{children}</AppWrapper>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				port: '', // leave empty
+				pathname: '/**', // allow all paths under this host
+			},
+		],
+	},
 };
 
 export default nextConfig;
